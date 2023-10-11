e824434664eb9b509e872dbd1e9a74541f18fd14
ziaFBacht <147515001+ziaFBacht@users.noreply.github.com>
2023-10-11 19:18:50 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string hw;
+
+            hw = "Hello World!";
+
+            Console.WriteLine(hw);
+            Console.WriteLine(hw);
+            Console.WriteLine(hw);
+        }
+    }
+}
