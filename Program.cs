5357e7a0abff71ae846a96995abedc1de7c9d4da
ziaFBacht <147515001+ziaFBacht@users.noreply.github.com>
2023-10-11 19:59:03 +0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,18 +1,79 @@
-﻿using System;
+// deklarasi pustaka
+using System;
 
+// bagian class
 namespace HelloWorld
 {
     class Program
     {
+        // bagian fungsi
         static void Main(string[] args)
         {
             string hw;
+            hw = "Hello String!"; // variable
+            
+            /*
+            Komentar
+            */
 
-            hw = "Hello World!";
+            Console.WriteLine(hw);
 
-            Console.WriteLine(hw);
-            Console.WriteLine(hw);
-            Console.WriteLine(hw);
+            string txt = "ini adalah manipulasi string";
+            Console.WriteLine(txt.ToUpper()); //menjadikan semua huruf menjadi uppercase
+
+            // operator aritmatika
+            var hasilarit = 5+2;
+            Console.WriteLine(hasilarit);
+
+            int num = 5; // operator penegasan
+            Console.WriteLine(++num);
+            Console.WriteLine(--num);
+
+            // array
+            string[] iniadalah = {"ini", "adalah", "array"}; // dari kiri: 0, 1, 2
+            Console.WriteLine(iniadalah[0]);
+            Console.WriteLine(iniadalah[1]);
+            Console.WriteLine(iniadalah[2]);
+
+            // list
+            var listname = new List<string> {"dan ini", "namanya", "list"};
+            for (int i = 0; i < listname.Count; i++)
+            {
+                Console.Write(listname[i] + " ");
+            }
+
+            // pengkondisian
+            int tiga = 3;
+            if (tiga == 3)
+            {
+                Console.WriteLine("tiga");
+            }
+
+            // ternary operator
+            int angkatenop = 10;
+            string hasiltenop = (angkatenop % 2==0)?"Genap":"Ganjil";
+            Console.WriteLine(hasiltenop);
+
+            // perulangan
+            int x = 1;
+
+            while (x < 5)
+            {
+                x += 1; // jangan lupa ininya :D
+                Console.WriteLine("ini akan tampil 5 kali");
+            }
+
+            // function
+
+            Tampil();
+
+            
+        }
+     public static void Tampil()
+         {
+            Console.WriteLine("ini akan ditampilkan lewat function");
         }
     }
 }
+
+//A11.2023.15044
